ca7ecaf2b7a8c3eb1f795f7a51ce553341acc3fa
juangil991 <jcgo125@gmail.com>
2022-02-13 11:21:12 -0500

    Se agrega el codigo de las funciones up y down para actualizar la posicon de las barras

--- Ping_Pong/main.js
+++ Ping_Pong/main.js
@@ -25,13 +25,17 @@
         this.board=board;
         this.board.bars.push(this);
         this.kind="rectangle";
+        this.speed = 10;
 
         self.Bar.prototype={
             down: function(){
-
+                this.y += this.speed;
             },
             up: function(){
-
+                this.y-= this.speed;
+            },
+            toString: function(){
+                return "x: "+ this.x + " y: "+ this.y;
             }
         }
     }
@@ -67,14 +71,24 @@
 
 })();
 
-self.addEventListener("load",main);
-
-function main(){
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100,board);
 var bar = new Bar(735,100,40,100,board);
 var canvas=document.getElementById('canvas');
 var board_view=new BoardView(canvas,board); 
+
+document.addEventListener("keydown",function(ev){
+    if(ev.keyCode==38){
+        bar.up();
+    }
+    else if(ev.keyCode==40){
+        bar.down();
+    }
+    console.log(""+bar);
+});
+self.addEventListener("load",main);
+
+function main(){
 board_view.draw();
 
 }
